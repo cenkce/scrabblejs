186c00962f953746bb4869c2b77715584b704ec2
Cenk Cetinkaya <mail@cenkcetinkaya.com>
2020-04-24 01:36:49 +0300

    feat: Make board centered

--- src/modules/game/GameBoardShape.tsx
+++ src/modules/game/GameBoardShape.tsx
@@ -2,10 +2,8 @@ import { Sprite, Graphics } from "pixi.js";
 import { BoardMap, CellColors } from "./BoardMapData";
 export class GameBoardShape extends Sprite {
     private graphics: Graphics = new Graphics();
-    constructor(cellSize: number) {
+    constructor(cellSize: number, borderSize: number, colCount = 15, rowCount = 15) {
         super();
-        const colCount = 15;
-        const rowCount = 15;
         const cellCount = colCount * rowCount;
         for (let i = 0; i < cellCount; i++) {
             let x = i % colCount;
@@ -13,10 +11,21 @@ export class GameBoardShape extends Sprite {
             let color = BoardMap[x][y];
             this.graphics
                 .beginFill(CellColors[color])
-                .lineStyle(2, 0x000000)
+                .lineStyle(borderSize, 0x000000)
                 .drawRect(x * cellSize, y * cellSize, cellSize, cellSize)
                 .endFill();
+            this.graphics.x = -this.graphics.width/2
+            this.graphics.y = -this.graphics.height/2
         }
+
         this.addChild(this.graphics);
+    }
+
+    get width() {
+        return this.graphics.width;
+    }
+
+    get height() {
+        return this.graphics.height;
     }
 }
--- src/modules/game/constants.ts
+++ src/modules/game/constants.ts
@@ -1,1 +1,3 @@
 export const CELL_SIZE = 100;
+export const CELL_BORDER_SIZE = 2;
+export const CELL_CONTENT_SIZE = CELL_SIZE - CELL_BORDER_SIZE * 2;
